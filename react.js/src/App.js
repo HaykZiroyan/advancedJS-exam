84c53a28eed32f7577d9de859722c6c6d72a6be7
Hayk Ziroyan <haik.ziroan@gmail.com>
2024-02-03 16:44:43 +0400

    modal component

--- react.js/src/App.js
+++ react.js/src/App.js
@@ -1,39 +1,16 @@
 import './App.css';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as yup from "yup";
+import Modal from './components/modal/modal';
+import { useState } from "react";
 
-function App() {
-
+function App() {  
+  [position, setPosition] = useState(false)
+  const change = () => {
+    setPosition(!position)
+  }
   return (
     <div className="App">
-        <Formik
-          initialValues={{ email: '', username: '' }}
-          onSubmit={values => alert(JSON.stringify(values, null, 2))}
-    >
-      {({ errors, touched }) => (
-        <Form>
-          <Field validate={""} name="text" type="text" />
-            {errors.email && touched.email ? <div>{errors.email}</div> : null}
-          <br />
-          <Field validate={""} name="comment" as="textarea" />
-              {errors.username && touched.username ? (
-                <div>{errors.username}</div>
-            ) : null}
-          <br />
-          
-          <Field validate={""} name="text" as="select"> 
-            <option value="">Please select a job type</option>
-            <option value="developer">Developer</option>
-            <option value="designer">Designer</option>
-            <option value="manager">Product Manager</option>
-            <option value="other">Other</option>
-          </Field>
-          <br />
-
-          <button type="submit">Submit</button>
-        </Form>
-        )}
-      </Formik>
+        <Modal onClick = {change} />
+        
     </div>
   );
 }
---
+++ react.js/src/components/modal/modal.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from "./modal.module.css"
+const Modal = () => {
+  return (
+    <button className={`${styles.btn}`}> switch </button>
+  )
+}
+
+export default Modal
